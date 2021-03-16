caf712de90b37e4479a7bdffbdb3b1e628576be5
Eyap53 <Eyap53@users.noreply.github.com>
2021-03-16 14:24:11 +0000

    [Bot] Automated dotnet-format update

--- Unity_Project/Assets/Scripts/Input/GameInput.cs
+++ Unity_Project/Assets/Scripts/Input/GameInput.cs
@@ -8,10 +8,10 @@ using UnityEngine.InputSystem.Utilities;
 
 public class @GameInput : IInputActionCollection, IDisposable
 {
-    public InputActionAsset asset { get; }
-    public @GameInput()
-    {
-        asset = InputActionAsset.FromJson(@"{
+	public InputActionAsset asset { get; }
+	public @GameInput()
+	{
+		asset = InputActionAsset.FromJson(@"{
     ""name"": ""GameInput"",
     ""maps"": [
         {
@@ -99,98 +99,99 @@ public class @GameInput : IInputActionCollection, IDisposable
         }
     ]
 }");
-        // Gameplay
-        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
-        m_Gameplay_Navigation = m_Gameplay.FindAction("Navigation", throwIfNotFound: true);
-    }
+		// Gameplay
+		m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+		m_Gameplay_Navigation = m_Gameplay.FindAction("Navigation", throwIfNotFound: true);
+	}
 
-    public void Dispose()
-    {
-        UnityEngine.Object.Destroy(asset);
-    }
+	public void Dispose()
+	{
+		UnityEngine.Object.Destroy(asset);
+	}
 
-    public InputBinding? bindingMask
-    {
-        get => asset.bindingMask;
-        set => asset.bindingMask = value;
-    }
+	public InputBinding? bindingMask
+	{
+		get => asset.bindingMask;
+		set => asset.bindingMask = value;
+	}
 
-    public ReadOnlyArray<InputDevice>? devices
-    {
-        get => asset.devices;
-        set => asset.devices = value;
-    }
+	public ReadOnlyArray<InputDevice>? devices
+	{
+		get => asset.devices;
+		set => asset.devices = value;
+	}
 
-    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+	public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
 
-    public bool Contains(InputAction action)
-    {
-        return asset.Contains(action);
-    }
+	public bool Contains(InputAction action)
+	{
+		return asset.Contains(action);
+	}
 
-    public IEnumerator<InputAction> GetEnumerator()
-    {
-        return asset.GetEnumerator();
-    }
+	public IEnumerator<InputAction> GetEnumerator()
+	{
+		return asset.GetEnumerator();
+	}
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
+	IEnumerator IEnumerable.GetEnumerator()
+	{
+		return GetEnumerator();
+	}
 
-    public void Enable()
-    {
-        asset.Enable();
-    }
+	public void Enable()
+	{
+		asset.Enable();
+	}
 
-    public void Disable()
-    {
-        asset.Disable();
-    }
+	public void Disable()
+	{
+		asset.Disable();
+	}
 
-    // Gameplay
-    private readonly InputActionMap m_Gameplay;
-    private IGameplayActions m_GameplayActionsCallbackInterface;
-    private readonly InputAction m_Gameplay_Navigation;
-    public struct GameplayActions
-    {
-        private @GameInput m_Wrapper;
-        public GameplayActions(@GameInput wrapper) { m_Wrapper = wrapper; }
-        public InputAction @Navigation => m_Wrapper.m_Gameplay_Navigation;
-        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
-        public void Enable() { Get().Enable(); }
-        public void Disable() { Get().Disable(); }
-        public bool enabled => Get().enabled;
-        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
-        public void SetCallbacks(IGameplayActions instance)
-        {
-            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
-            {
-                @Navigation.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnNavigation;
-                @Navigation.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnNavigation;
-                @Navigation.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnNavigation;
-            }
-            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
-            if (instance != null)
-            {
-                @Navigation.started += instance.OnNavigation;
-                @Navigation.performed += instance.OnNavigation;
-                @Navigation.canceled += instance.OnNavigation;
-            }
-        }
-    }
-    public GameplayActions @Gameplay => new GameplayActions(this);
-    private int m_VRSetupSchemeIndex = -1;
-    public InputControlScheme VRSetupScheme
-    {
-        get
-        {
-            if (m_VRSetupSchemeIndex == -1) m_VRSetupSchemeIndex = asset.FindControlSchemeIndex("VR-Setup");
-            return asset.controlSchemes[m_VRSetupSchemeIndex];
-        }
-    }
-    public interface IGameplayActions
-    {
-        void OnNavigation(InputAction.CallbackContext context);
-    }
+	// Gameplay
+	private readonly InputActionMap m_Gameplay;
+	private IGameplayActions m_GameplayActionsCallbackInterface;
+	private readonly InputAction m_Gameplay_Navigation;
+	public struct GameplayActions
+	{
+		private @GameInput m_Wrapper;
+		public GameplayActions(@GameInput wrapper) { m_Wrapper = wrapper; }
+		public InputAction @Navigation => m_Wrapper.m_Gameplay_Navigation;
+		public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+		public void Enable() { Get().Enable(); }
+		public void Disable() { Get().Disable(); }
+		public bool enabled => Get().enabled;
+		public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+		public void SetCallbacks(IGameplayActions instance)
+		{
+			if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+			{
+				@Navigation.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnNavigation;
+				@Navigation.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnNavigation;
+				@Navigation.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnNavigation;
+			}
+			m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+			if (instance != null)
+			{
+				@Navigation.started += instance.OnNavigation;
+				@Navigation.performed += instance.OnNavigation;
+				@Navigation.canceled += instance.OnNavigation;
+			}
+		}
+	}
+	public GameplayActions @Gameplay => new GameplayActions(this);
+	private int m_VRSetupSchemeIndex = -1;
+	public InputControlScheme VRSetupScheme
+	{
+		get
+		{
+			if (m_VRSetupSchemeIndex == -1)
+				m_VRSetupSchemeIndex = asset.FindControlSchemeIndex("VR-Setup");
+			return asset.controlSchemes[m_VRSetupSchemeIndex];
+		}
+	}
+	public interface IGameplayActions
+	{
+		void OnNavigation(InputAction.CallbackContext context);
+	}
 }
